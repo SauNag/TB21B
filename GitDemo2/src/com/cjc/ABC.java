7ff8df7c87d5a309fb84c0f1dc043d5bf3746ea9
user <user@DESKTOP-5CJ1P84>
2023-08-21 15:04:45 +0530

    By saurabh 2

---
+++ GitDemo2/src/com/cjc/ABC.java
@@ -0,0 +1,11 @@
+package com.cjc;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Git demo 2 ");
+System.out.println("Git aaaaaa");
+	}
+
+}
